8e988be73811bb92feff57c6fee2d10aca815635
robksawyer <robksawyer@gmail.com>
2014-11-15 15:46:54 -0800

    Added new views

---
+++ api/controllers/SearchController.js
@@ -0,0 +1,15 @@
+/**
+ * SearchController
+ *
+ * @description :: Server-side logic for managing searches
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+	index: function( req, res){
+		console.log("Hi!");
+        res.view();
+	}
+};
+
