1bb60f162d8be95cd2c65fa090f7ccc919d60012
robksawyer <robksawyer@gmail.com>
2014-11-15 12:40:56 -0800

    first commit

--- config/passport.js
+++ config/passport.js
@@ -20,25 +20,25 @@ module.exports.passport = {
     strategy: require('passport-local').Strategy
   },
 
-  twitter: {
-    name: 'Twitter',
-    protocol: 'oauth',
-    strategy: require('passport-twitter').Strategy,
-    options: {
-      consumerKey: 'your-consumer-key',
-      consumerSecret: 'your-consumer-secret'
-    }
-  },
+  // twitter: {
+  //   name: 'Twitter',
+  //   protocol: 'oauth',
+  //   strategy: require('passport-twitter').Strategy,
+  //   options: {
+  //     consumerKey: 'your-consumer-key',
+  //     consumerSecret: 'your-consumer-secret'
+  //   }
+  // },
 
-  github: {
-    name: 'GitHub',
-    protocol: 'oauth2',
-    strategy: require('passport-github').Strategy,
-    options: {
-      clientID: 'your-client-id',
-      clientSecret: 'your-client-secret'
-    }
-  },
+  // github: {
+  //   name: 'GitHub',
+  //   protocol: 'oauth2',
+  //   strategy: require('passport-github').Strategy,
+  //   options: {
+  //     clientID: 'your-client-id',
+  //     clientSecret: 'your-client-secret'
+  //   }
+  // },
 
   facebook: {
     name: 'Facebook',
@@ -50,10 +50,10 @@ module.exports.passport = {
     }
   },
 
-  google: {
-    name: 'Google',
-    protocol: 'openid',
-    strategy: require('passport-google').Strategy
-  }
+  // google: {
+  //   name: 'Google',
+  //   protocol: 'openid',
+  //   strategy: require('passport-google').Strategy
+  // }
 
 };
