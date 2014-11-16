08c865142aae18ede7f7fdaf220c8f97c9330040
robksawyer <robksawyer@gmail.com>
2014-11-16 03:27:45 -0800

    Added mongodb to the connections

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'localPostgresqlServer'
+    connection: 'herokuMongodbServer'
   }
 
 };
